b8a63cf9d6d1a25fffa548a6f148b035cec21dd2
Frederik Goovaerts <frederik.goovaerts@theoplayer.com>
2019-11-16 20:47:58 +0100

    Add firebase library

---
+++ src/firebase/main.ts
@@ -0,0 +1,17 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
