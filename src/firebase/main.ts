ce5260905492a7a051b65488cb6de88eedf6ed71
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-11-17 00:41:57 +0100

    Add example auth widget

--- src/firebase/main.ts
+++ src/firebase/main.ts
@@ -1,7 +1,19 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import { auth } from "firebaseui";
+
 import "firebase/firestore";
 
 import { FIREBASE_CONFIG } from "../configuration/firebase";
 
 firebase.initializeApp(FIREBASE_CONFIG);
+const ui = new auth.AuthUI(firebase.auth());
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+    },
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+});
